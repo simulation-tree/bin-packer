fd1c6bf649da0d8d4441d6a4e8e1a88b3d8a64af
Phill <philldamaskin@gmail.com>
2025-01-15 18:23:00 -0500

    remove dependence on collections

--- source/RecursivePacker.cs
+++ source/RecursivePacker.cs
@@ -1,5 +1,4 @@
-﻿using Collections;
-using RectpackSharp;
+﻿using RectpackSharp;
 using System.Numerics;
 using Unmanaged;
 
@@ -9,7 +8,8 @@ namespace BinPacker
     {
         void IBinPacker.Pack(USpan<Vector2> sizes, USpan<Vector2> positions, Vector2 maxSize, Vector2 padding)
         {
-            using List<PackingRectangle> boxes = new(sizes.Length);
+            USpan<PackingRectangle> boxes = stackalloc PackingRectangle[(int)sizes.Length];
+            uint boxesCount = 0;
             for (uint i = 0; i < sizes.Length; i++)
             {
                 Vector2 size = sizes[i];
@@ -19,7 +19,7 @@ namespace BinPacker
                     box.Width = (uint)(size.X + padding.X * 2);
                     box.Height = (uint)(size.Y + padding.Y * 2);
                     box.Id = (int)i;
-                    boxes.Add(box);
+                    boxes[boxesCount++] = box;
                 }
             }
 
@@ -30,14 +30,14 @@ namespace BinPacker
             uint stepSize = 1;
             try
             {
-                RectanglePacker.Pack(boxes.AsSpan().AsSystemSpan(), out PackingRectangle bounds, hint, acceptableDensity, stepSize, maxWidth, maxHeight);
+                RectanglePacker.Pack(boxes.Slice(0, boxesCount).AsSystemSpan(), out PackingRectangle bounds, hint, acceptableDensity, stepSize, maxWidth, maxHeight);
             }
             catch
             {
                 throw new ImpossibleFitException($"Not enough space available to pack the given sizes within the bounds of {maxSize}.");
             }
 
-            for (uint i = 0; i < boxes.Count; i++)
+            for (uint i = 0; i < boxesCount; i++)
             {
                 PackingRectangle box = boxes[i];
                 uint index = (uint)box.Id;
